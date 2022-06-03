8688690a68d749ba7ec896966bd9869137a70a06
Christopher Marcotte <christopher.marcotte+git@gmail.com>
2022-06-03 15:34:57 +0100

    Delete dynamicsMovie.sh

--- dynamicsMovie.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ./dynamics_frames
-python dynamicsPlot.py --workDir $1
-
-rm ${1}_dynamics.mp4
-ffmpeg -r 10 -f image2 -start_number 1 -i ./dynamics_frames/%04d.png -c:v h264_nvenc -pix_fmt yuv420p -loglevel quiet -stats ${1}_dynamics.mp4
-rm -r ./dynamics_frames
